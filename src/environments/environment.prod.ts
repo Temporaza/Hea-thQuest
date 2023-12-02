bfb73c00ff675ff223a3d93736be2d016db38cbd
Ryan Temporaza <dreiyn.temp@gmail.com>
2023-12-02 15:01:11 +0800

    02-12-2023 second

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAO_sUPex51FoaHP76UtrYiENgTxW6KEvI",
+    authDomain: "auth-example-99647.firebaseapp.com",
+    projectId: "auth-example-99647",
+    storageBucket: "auth-example-99647.appspot.com",
+    messagingSenderId: "1098654882342",
+    appId: "1:1098654882342:web:aab6b15486962c0111e4e7",
+    measurementId: "G-N2K6B094FY"
+  }
 };
